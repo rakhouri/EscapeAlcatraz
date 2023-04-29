674527f44df128472c06107c88049247edf2d9ad
Richard A Khouri Jr <rkhouri@babbage15.computing.clemson.edu>
2023-04-29 12:17:10 -0400

    Finished the Room.java class

--- Room.java
+++ Room.java
@@ -5,13 +5,76 @@ public class Room {
 
     private String name;
     private String description;
-    ArrayList<String> items = new ArrayList<String>();
+    ArrayList<Item> items = new ArrayList<String>();
     ArrayList<String> exits = new ArrayList<String>();
 
+    /**
+     * Room constructor that initializes the room
+     * @param name- The name of the room
+     * @param description- The description that describes the room
+     */
     public Room(String name, String description) {
         this.name = name;
         this.description = description
     }
+
+    /**
+     * Returns the name of the room
+     * @return- The name of the room
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Returns the description of the room
+     * @return- The description of the room
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * Adds an exit to the room's exit ArrayList
+     * @param exit- The name of the exit
+     */
+    public void addExit(String exit) {
+        exits.add(exit);
+    }
+
+    /**
+     * List's all of the possible exits of the room
+     * @return- The string of exits
+     */
+    public String listExits() {
+
+        String exitString = "Exits:\n";
+        for (String s : exits) {
+            exitString = exitString + s + "\n";
+        }
+
+        return exitString;
+    }
+
+    /**
+     * Adds an item to the room's items ArrayList
+     * @param item- The item object that is being added to the ArrayList
+     */
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    /**
+     * Creates a string representation of the room, description, and all possible exits
+     * @return- A string of the name of the room, description, and list of exits
+     */
+    public String toString() {
+
+        String roomToString = this.name + ": " + this.description + "\n" + "\n" + listExits();
+        return roomToString;
+    }
+
+
     
     
 
